be1bc8a358683bb1609bea48cf25dd957187e5e2
mohsen-hussen <mohsenhussen8@gmail.com>
2022-08-27 22:39:39 +0200

    add reusable textField component and use it at layout

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,59 @@
 import React from "react";
+import { Formik, Form } from "formik";
+import * as Yup from "yup";
+import { Container, Grid, Typography } from "@mui/material";
+
+import Textfield from "../components/FormsUI/Textfield";
+
+const INITIAL_FORM_STATE = {
+	fullName: "",
+	businessEmail: "",
+};
+
+const FORM_VALIDATION = Yup.object().shape({
+	fullName: Yup.string().required("Required"),
+	businessEmail: Yup.string().email("Invalid email.").required("Required"),
+});
 
 const Home = () => {
-	return <div>Home</div>;
+	return (
+		<Formik
+			initialValues={{
+				...INITIAL_FORM_STATE,
+			}}
+			validationSchema={FORM_VALIDATION}
+			onSubmit={(values) => {
+				console.log(values);
+			}}
+		>
+			<Form>
+				<Grid container spacing={2}>
+					<Grid item xs={12}>
+						<Typography
+							variant="h5"
+							sx={{ display: "flex", justifyContent: "center" }}
+						>
+							Tell us more about you.
+						</Typography>
+					</Grid>
+					<Grid item xs={12}>
+						<Textfield
+							name="fullName"
+							label="Full Name"
+							placeholder="Enter Your Full Name"
+						/>
+					</Grid>
+					<Grid item xs={12}>
+						<Textfield
+							name="businessEmail"
+							label="Business Email"
+							placeholder="Enter Your Business Email"
+						/>
+					</Grid>
+				</Grid>
+			</Form>
+		</Formik>
+	);
 };
 
 export default Home;
