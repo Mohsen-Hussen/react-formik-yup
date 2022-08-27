88a4a7411dcc2c4b2b0562464f66087384548b9f
mohsen-hussen <mohsenhussen8@gmail.com>
2022-08-28 00:09:48 +0200

    add reusable select component and use it at layout

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,30 @@
 import React from "react";
 import { Formik, Form } from "formik";
 import * as Yup from "yup";
-import { Container, Grid, Typography } from "@mui/material";
+import { Grid, Typography } from "@mui/material";
 
 import Textfield from "../components/FormsUI/Textfield";
+import Select from "../components/FormsUI/Select";
+import countries from "../data/countries.json";
+import countriesKeys from "../data/countries-keys.json";
 
 const INITIAL_FORM_STATE = {
-	fullName: "",
-	businessEmail: "",
+	userFullName: "",
+	userBusinessEmail: "",
+	userCountry: "",
+	userCountryKey: "",
+	userPhone: "",
 };
 
 const FORM_VALIDATION = Yup.object().shape({
-	fullName: Yup.string().required("Required"),
-	businessEmail: Yup.string().email("Invalid email.").required("Required"),
+	userFullName: Yup.string().required("Required"),
+	userBusinessEmail: Yup.string().email("Invalid email.").required("Required"),
+	userCountry: Yup.string().required("Required"),
+	userCountryKey: Yup.string().required("Required"),
+	userPhone: Yup.number()
+		.integer()
+		.typeError("Please enter a valid phone number")
+		.required("Required"),
 });
 
 const Home = () => {
@@ -26,7 +38,7 @@ const Home = () => {
 				console.log(values);
 			}}
 		>
-			<Form>
+			<Form autoComplete="off">
 				<Grid container spacing={2}>
 					<Grid item xs={12}>
 						<Typography
@@ -38,17 +50,42 @@ const Home = () => {
 					</Grid>
 					<Grid item xs={12}>
 						<Textfield
-							name="fullName"
+							name="userFullName"
 							label="Full Name"
 							placeholder="Enter Your Full Name"
 						/>
 					</Grid>
 					<Grid item xs={12}>
 						<Textfield
-							name="businessEmail"
+							name="userBusinessEmail"
 							label="Business Email"
 							placeholder="Enter Your Business Email"
 						/>
+					</Grid>
+					<Grid item xs={12} sm={6}>
+						<Select
+							name="userCountry"
+							label="Choose Your Country"
+							options={countries}
+						/>
+					</Grid>
+					<Grid item xs={12} sm={6}>
+						<Grid container>
+							<Grid item xs={3}>
+								<Select
+									name="userCountryKey"
+									label="Country Key"
+									options={countriesKeys}
+								/>
+							</Grid>
+							<Grid item xs={9}>
+								<Textfield
+									name="userPhone"
+									label="Phone Number"
+									placeholder="Enter Your Phone Number"
+								/>
+							</Grid>
+						</Grid>
 					</Grid>
 				</Grid>
 			</Form>
